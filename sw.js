2c4027fb11f43daa2523ef299fcedfda4a955b75
tulikaff659 <tolqinjonofficel@gmail.com>
2025-12-25 01:25:59 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,16 +1,8 @@
-// Service Worker for Apple of Fortune
-const CACHE_NAME = 'apple-fortune-v1.0';
-
-self.addEventListener('install', event => {
+// sw.js - oddiy versiya
+self.addEventListener('install', function(event) {
   console.log('Service Worker installing');
-  event.waitUntil(self.skipWaiting());
 });
 
-self.addEventListener('activate', event => {
-  console.log('Service Worker activated');
-  event.waitUntil(self.clients.claim());
-});
-
-self.addEventListener('fetch', event => {
-  event.respondWith(fetch(event.request));
+self.addEventListener('fetch', function(event) {
+  // Bo'sh - cache qilmaydi
 });
